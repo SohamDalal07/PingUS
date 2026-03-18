ce10d2578d2644f40012e071fb2f575306e22211
Your Name <ask.atharva2004@gmail.com>
2026-03-19 01:22:12 +0530

    home page with navigation to post page alert page and profile page via intent

---
+++ app/src/main/java/com/example/backtoyou/Home.java
@@ -0,0 +1,49 @@
+package com.example.backtoyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_post) {
+                startActivity(new Intent(Home.this, PostActivity.class));
+                return true;
+            } else if (itemId == R.id.navigation_alerts) {
+                startActivity(new Intent(Home.this, AlertsActivity.class));
+                return true;
+            }
+            else if (itemId == R.id.navigation_profile) {
+                startActivity(new Intent(Home.this, AlertsActivity.class));
+                return true;}
+
+            else if (itemId == R.id.navigation_home) {
+                // Already on Home
+                return true;
+            }
+            return false;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
